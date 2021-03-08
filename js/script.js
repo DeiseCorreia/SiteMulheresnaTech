f0cc59c8353ead456ce833ccb3f196ac312936b3
Deise Correia <deisecorreia34@gmail.com>
2021-03-08 02:39:55 +0000

    Projeto Finalizado!

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+document.getElementById("Botaoenviar").addEventListener("click",validarFormulario )
+
+
+function validarFormulario(){
+
+  if(document.getElementById("nome").value != "" &&             document.getElementById("email").value != "" && document.getElementById("telefone").value){
+
+    alert("Pronto! Você receberá mais novidades pelo e-mail :)!")
+
+  }else{
+
+    alert("Falta um campo á ser preenchido!")
+
+  }
+}
